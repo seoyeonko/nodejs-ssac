1a9b30da3e8cd6b6b6d16db181e256efc7ccb910
seoyeonko <seoyeon99o@naver.com>
2021-10-29 00:41:41 +0900

    채팅 v.1

--- app_socket.js
+++ app_socket.js
@@ -12,20 +12,22 @@ app.get('/', (req, res) => {
   res.render('socket');
 });
 
+let nick_array = [];
+
 // io.on(): 서버에서의 소켓 연결 (셋팅)
 // socket.emit(): 보낼 때
 // socket.on(): 받을 때
 io.on('connection', function (socket) {
   // socket이 연결되면 클라이언트가 이 내부 작업을 함
   console.log('Socket connected');
+  io.emit('noticeIn', { notice: `${socket.id}님이 입장했습니다.` });
 
   socket.on('sendMsg', (msg) => {
-    // io.emit('newMsg', `${socket.id}님: ${msg}`);
-    io.emit('newMsg', { socketid: socket.id, message: msg });
+    io.emit('newMsg', { socketid: msg['nickname'], message: msg['message'] });
   });
 
   socket.on('disconnect', () => {
-    io.emit('notice', `${socket.id}님 나감`);
+    io.emit('noticeOut', { notice: `${socket.id}님 퇴장했습니다.` });
   });
 });
 
