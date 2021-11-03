bcfee7d61120692f2f9d5eb4f69bc075ea23a218
seoyeonko <seoyeon99o@naver.com>
2021-11-03 17:04:10 +0900

    퇴장 공지 이슈 해결

--- app_socket.js
+++ app_socket.js
@@ -6,7 +6,7 @@ const io = require('socket.io')(http);
 
 const PORT = 8000; // PORT number
 let userNick;
-let userList = {}; // 배열 원소: { socketid, nickname }
+let userList = {}; // 배열 원소: { socketid : nickname, ... }
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -45,11 +45,14 @@ io.on('connection', function (socket) {
 
   // userList
   userList[socket.id] = userNick; // 리스트에 추가
-  console.log(userNick + '님!!!! 입자아앙');
+  console.log(userList[socket.id] + '님!!!! 입자아앙');
   console.log(userList);
 
   io.emit('noticeIn', {
-    notice: `${userNick}(${socket.id.slice(0, 5)})님이 입장했습니다.`,
+    notice: `${userList[socket.id]}(${socket.id.slice(
+      0,
+      5
+    )})님이 입장했습니다.`,
     userList: userList,
   });
 
@@ -69,24 +72,19 @@ io.on('connection', function (socket) {
   });
 
   socket.on('disconnect', () => {
-    // userList 업데이트
-    // userList = userList.filter((element) => {
-    //   // 배열 각 원소의 socketid !== 나가려는 브라우저의 socket.id; 배열에 남겨둠
-    //   if (element.socketid !== socket.id) {
-    //     return true;
-    //   }
-    // });
-    // console.log(userList);
-
-    console.log(userList[socket.id] + '님!!!! 퇴자아앙');
-    delete userList[socket.id]; // 리스트에서 삭제
-    console.log(userList);
-
+    // 퇴장 공지
     io.emit('noticeOut', {
       notice: `${userList[socket.id]}(${socket.id.slice(
         0,
         5
       )})님이 퇴장했습니다.`,
+    });
+
+    // 리스트 업데이트
+    console.log(userList[socket.id] + '님!!!! 퇴자아앙');
+    delete userList[socket.id]; // 리스트에서 삭제
+    console.log(userList);
+    io.emit('updateList', {
       userList: userList,
     });
   });
